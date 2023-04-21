7e1ea812a2f17ac9c7a83d3d7808934baa545dab
Antonio Sanchez <antonio.sanchez@qvantel.com>
2023-04-21 12:19:09 +0300

    Playground with spring boot and kafka reactor

---
+++ src/main/java/com/example/kafkareactor/KafkaReactorPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkareactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaReactorPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaReactorPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkareactor/KafkaReactorPlaygroundApplicationTests.java
@@ -0,0 +1,125 @@
+package com.example.kafkareactor;
+
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+@SpringBootTest
+class KafkaReactorPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void simpleFluxExample() {
+        Flux<String> fluxColors = Flux.just("red", "green", "blue");
+        fluxColors.subscribe(System.out::println);
+    }
+
+
+    @Test
+    void zipExample() {
+        Flux<String> fluxFruits = Flux.just("apple", "pear", "plum");
+        Flux<String> fluxColors = Flux.just("red", "green", "blue");
+        Flux<Integer> fluxAmounts = Flux.just(10, 20, 30);
+        Flux.zip(fluxFruits, fluxColors, fluxAmounts).subscribe(System.out::println);
+    }
+
+    @Test
+
+
+    public void onErrorExample() {
+        Flux<String> fluxCalc = Flux.just(-1, 0, 1)
+                .map(i -> "10 / " + i + " = " + (10 / i));
+        fluxCalc.subscribe(value -> System.out.println("Next: " + value),
+                error -> System.err.println("Error: " + error));
+    }
+
+    @Test
+
+
+    public void publishSubscribeExample() {
+        Scheduler schedulerA = Schedulers.newParallel("Scheduler A");
+        Scheduler schedulerB = Schedulers.newParallel("Scheduler B");
+        Scheduler schedulerC = Schedulers.newParallel("Scheduler C");
+
+        Flux.just(1)
+                .map(i -> {
+                    System.out.println("First map: " + Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(schedulerA)
+                .map(i -> {
+                    System.out.println("Second map: " + Thread.currentThread().getName());
+                    return i;
+                })
+                .publishOn(schedulerB)
+                .map(i -> {
+                    System.out.println("Third map: " + Thread.currentThread().getName());
+                    return i;
+                })
+                .subscribeOn(schedulerC)
+                .map(i -> {
+                    System.out.println("Fourth map: " + Thread.currentThread().getName());
+                    return i;
+                })
+                .publishOn(schedulerA)
+
+                .map(i -> {
+                    System.out.println("Fifth map: " + Thread.currentThread().getName());
+                    return i;
+                })
+                .blockLast();
+    }
+
+    @Test
+
+
+    public void backpressureExample() {
+
+        Flux.range(1, 5)
+                .subscribe(new Subscriber<Integer>() {
+                    private Subscription s;
+                    int counter;
+
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        System.out.println("onSubscribe");
+                        this.s = s;
+                        System.out.println("Requesting 2 emissions");
+                        s.request(2);
+                    }
+
+                    @Override
+
+                    public void onNext(Integer i) {
+                        System.out.println("onNext " + i);
+                        counter++;
+
+                        if (counter % 2 == 0) {
+                            System.out.println("Requesting 2 emissions");
+                            s.request(2);
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        System.err.println("onError");
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("onComplete");
+                    }
+                });
+
+
+    }
+
+
+}
